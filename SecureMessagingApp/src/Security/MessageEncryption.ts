ae9004dd1002f2eaa9b93ad5702a1e830324ae8f
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-11-14 21:34:07 -0700

    some updates

--- SecureMessagingApp/src/Security/MessageEncryption.ts
+++ SecureMessagingApp/src/Security/MessageEncryption.ts
@@ -4,11 +4,16 @@ export class MessageEncryption {
   constructor(secretKey: string) {
     this.secretKey = secretKey;
   }
-
+  public getSecretKey(): string {
+    return this.secretKey;
+  }
   public encryptMessage(message: string): string {
     return CryptoJS.AES.encrypt(message, this.secretKey).toString();
   }
+  public decryptMessage(message: string): string {
+    return CryptoJS.AES.decrypt(message, this.secretKey).toString(
+      CryptoJS.enc.Utf8,
+    );
+  }
 }
 
-const messageEncryption = new MessageEncryption('secretKey');
-console.log(messageEncryption.encryptMessage('Hello World!'));
