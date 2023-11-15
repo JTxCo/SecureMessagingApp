859b496551c4780881472dacb5ea0792ad14c42b
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-11-15 09:53:32 -0700

    created User

--- SecureMessagingApp/src/Database/User.ts
+++ SecureMessagingApp/src/Database/User.ts
@@ -5,13 +5,16 @@ export class User {
     hashedPassword: string;
     publicKey: string;
   
-    constructor(id: number, username: string, password: string, publicKey: string) {
+    constructor(id: number, username: string, hashedPassword: string, publicKey: string) {
       this.id = id;
       this.username = username;
-      this.hashedPassword = hashPassword(password);
+      this.hashedPassword = hashedPassword;
       this.publicKey = publicKey;
     }
     // Include here any methods that are specific to each user
-
+    static async create(id: number, username: string, password: string, publicKey: string): Promise<User> {
+        const hashedPassword = await hashPassword(password);
+        return new User(id, username, hashedPassword, publicKey);
+    }
   }
   
