6d5efeb28c604821c9ab614998f41edecbffb02a
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-11-15 13:22:19 -0700

    updates

---
+++ SecureMessagingApp/src/Model-Structure/Model.ts
@@ -0,0 +1,19 @@
+import('./Security')
+ .then(({MessageEncryption, generateSecretKey, getSecretKey}) => {
+    (async () => {
+      await generateSecretKey();
+      const secretKey = await getSecretKey();
+      
+      if (secretKey === null) {
+        console.error('No secret key found. Please generate a new one.');
+        return;
+      }
+      const messageEncryption = new MessageEncryption(secretKey);
+    
+      const encryptedMessage = messageEncryption.encryptMessage('Hello World!');
+      console.log(encryptedMessage);
+    
+      const decryptedMessage = messageEncryption.decryptMessage(encryptedMessage);
+      console.log(decryptedMessage);
+    })();
+});
