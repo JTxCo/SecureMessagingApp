f4344f6ffba86d337b4f84cee3726e1bac2e2f6e
Joel Carlson <cjoeld2024@gmail.com>
2023-12-06 08:53:20 -0700

    added User object and adapted to ts

--- SecureMessagingApp/src/Database/MessageStatus/MessageStatus.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "../Message";
-export interface MessageStatus {
-    onEnter(message: Message): void;
-    onExit(message: Message): MessageStatus;
-    getStatus(): string;
-    
-  }
-  
--- SecureMessagingApp/src/Database/MessageStatus/index.ts
+++
@@ -1,9 +0,0 @@
-export {DeliveredStatus} from "./DeliveredStatus";
-export {ReadStatus} from "./ReadStatus";
-export {SentStatus} from "./SentStatus";
-export {DraftStatus} from "./DraftStatus";
-export {ErrorStatus} from "./ErrorStatus";
-export type {MessageStatus} from "./MessageStatus";
-export {createMessageStatus} from "./MessageStatusFactory";
-import { Message } from "../Message";
-export {Message} from "../Message";
