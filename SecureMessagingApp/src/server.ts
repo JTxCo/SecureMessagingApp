993f3d494d296c1f1c209e7390e3bdf66aa6bbe2
smileydavisl <smileydavisl@gmail.com>
2023-12-06 21:04:34 -0700

    sending messages through terminal

---
+++ SecureMessagingApp/src/server.ts
@@ -0,0 +1,31 @@
+// server.ts
+import * as WebSocket from 'ws';
+
+const wss = new WebSocket.Server({ port: 3000 });
+const clients: WebSocket[] = [];
+
+wss.on('connection', (ws: WebSocket) => {
+    clients.push(ws);
+
+    ws.on('message', (message: string) => {
+        try{    
+            const parsedMessage = JSON.parse(message);
+            console.log('Received: ', parsedMessage.message);
+            clients.forEach((client) => {
+                if (client !== ws){
+                    client.send(JSON.stringify(parsedMessage));
+                }
+            })
+        }
+        catch (error){
+            console.error('Error parsing message: ', error);
+        }
+    });
+
+    ws.on('close', () => {
+        // Remove the disconnected client
+        clients.splice(clients.indexOf(ws), 1);
+    });
+});
+
+console.log('WebSocket server is running on port 3000');
